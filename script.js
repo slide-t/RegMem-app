ae91fae0925c0bf2a44b1576abbf39d1e9a2844a
slide-t <slideteckacademy@gmail.com>
2025-07-20 21:26:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+<script>
+  function togglePartyFields() {
+    const partySelect = document.getElementById("partyMember");
+    const partyFields = document.getElementById("partyFields");
+    const partyIdInput = document.getElementById("partyId");
 
+    if (partySelect.value === "yes") {
+      partyFields.style.display = "block";
+      partyIdInput.setAttribute("required", "required");
+    } else {
+      partyFields.style.display = "none";
+      partyIdInput.removeAttribute("required");
+      partyIdInput.value = "";
+      document.getElementById("positionHeld").value = "";
+    }
+  }
+</script>
